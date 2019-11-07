3b7221e6b9fb4095fd242835344f4481da88674d
Mr.004 <57171003+Mr-004Tols@users.noreply.github.com>
2019-11-07 20:41:47 +0700

    Update whatshack.sh

--- whatshack.sh
+++ whatshack.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 clear
-echo "
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
 echo
 python2 meizu.py
 echo
 clear
+echo
 <-- Gamer 4 LIve -->
 ####################
 #   Whatsapp Code  # By Mr.004
